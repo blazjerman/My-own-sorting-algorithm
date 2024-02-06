80778fd473eade7fdb9d553cf8705e3d81a7162a
Blaz <blaz.jerman5@gmail.com>
2024-02-06 10:00:23 +0100

    Optimized code.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,7 @@
 #include <random>
 
 
-const int32_t ARRAY_SIZE = 200000000;
-uint32_t array[2][ARRAY_SIZE];
-
-
-
-void sortByBit(const int32_t start, const int32_t end, uint32_t bit) {
-
-    uint32_t *arrayRead = array[(bit & 0x55555555) != 0];
-    uint32_t *arrayWrite = array[(bit & 0x55555555) == 0];
+void sortByBit(uint32_t *arrayRead, uint32_t *arrayWrite, const int32_t start, const int32_t end, uint32_t bit) {
 
     int32_t top = start;
     int32_t bot = end - 1;
@@ -31,8 +23,8 @@ void sortByBit(const int32_t start, const int32_t end, uint32_t bit) {
     bit >>= 1;
     if (bit == 0) return;
 
-    if(top != start) sortByBit(start, top, bit);
-    if(end != top) sortByBit(top, end, bit);
+    if(top != start) sortByBit(arrayWrite, arrayRead, start, top, bit);
+    if(end != top) sortByBit(arrayWrite, arrayRead,top, end, bit);
 
 }
 
@@ -40,7 +32,15 @@ void sortByBit(const int32_t start, const int32_t end, uint32_t bit) {
 
 int main() {
 
+
+    const int32_t ARRAY_SIZE = 2000000000;
+    auto **array = new uint32_t*[2];
+
+    for (size_t i = 0; i < 2; ++i) array[i] = new uint32_t[ARRAY_SIZE];  // Allocate memory for each row
+
     uint32_t max_unsigned_int_size = std::numeric_limits<unsigned int>::max();
+
+
 
 
     //Random numbers
@@ -56,7 +56,7 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
 
-    sortByBit(0,ARRAY_SIZE - 1, 1 << 31);
+    sortByBit(array[0], array[1], 0,ARRAY_SIZE - 1, 1 << 31);
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
